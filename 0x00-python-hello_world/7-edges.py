fbb9a8c42bbbca93ebb5b79a1ce9f71ea9e2aeca
root <gisubizo.jovan@gmail.com>
2023-01-08 10:31:16 +0000

    updates commited

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = 'Holberton'
+first_word_3 = word[:3]
+last_word_2 = word[2:]
+middle_word = word[1:-1]
+print(f"first  3 letters: {first_word_3}")
+print(f"last 2 letters: {last_word_2}")
+print(f"middle word: {middle_word}")
